c5798e6bd2fe316cefabd044b788aed75de8aae4
aldafari <aldafari@memphis.edu>
2024-02-16 23:41:21 -0600

    Created error handling for problem 4

--- prob4.rb
+++ prob4.rb
@@ -6,7 +6,13 @@ File.open('input.txt', 'r') do |file|
       file.puts "#{new_line}"
     end
 
+  begin
+    File.open('input.txt','r') do |file|
+      puts file.read
+    end
+  rescue StandardError => e
+    puts "An error occured: #{e.message}"
+    end
   end
-
 
 end
