c85265f46bd465af6ee8df67f1db0b242e2af45a
aldafari <aldafari@memphis.edu>
2024-02-17 01:45:06 -0600

    Improved probem 6

--- prob6.rb
+++ prob6.rb
@@ -1,2 +1,4 @@
 student = {name: "Alice", age:20, grade:"A"}
-puts "Name: #{student[:name]}\nAge: #{student[:age]}\nGrade: #{student[:grade]}"
+student.each do |key, value|
+  puts "#{key.capitalize}: #{value}"
+end
